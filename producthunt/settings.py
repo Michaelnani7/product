52b477423546fff8f8f338b3d3c5deeea9f2aa2f
Adeyinka <adeyinkamichael95@gmail.com>
2020-02-04 11:54:47 -0800

    now

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'producthuntdb',
-        'USER': 'postgres',
-        'PASSWORD': 'django1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
